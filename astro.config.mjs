6a8c6eeb5375843c966c37c507c39ca92d58a8fc
KJ Roelke <kj.roelke@gmail.com>
2023-11-29 15:17:41 -0600

    deploy to github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: "standalone"
-  })
+	site: 'https://choctaw-nation/github.io',
+	base: 'wireframe-veterans-archive',
 });
